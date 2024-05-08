a51fffa05c2d2aaf4f07ff16e4fea73ac5a212f5
Relay90 <motaungr511@gmail.com>
2024-05-07 23:39:04 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    response = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
-                            headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subscribers = response.get("data", {}).get("subscribers", 0)
-    return subscribers
-
-
-if __name__ == "__main__":
-    pass  # You can add test code here if needed
+    results = response.json().get("data")
+    return results.get("subscribers")
